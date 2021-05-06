3ef84cc380d7681e76276f6bd330457da51c152d
Tiago <a90418@alunos.uminho.pt>
2021-05-06 14:43:10 +0100

    atualizacao 06/05 14h:40

---
+++ Frontend/Backoffice/Adaptado/controllers/controller_calendarioInstituicao.js
@@ -0,0 +1,44 @@
+window.onload = function () {
+  listarAtividades();
+};
+
+function listarAtividades() {
+  var nomeAtividade;
+
+  fetch("http://localhost:8080/DAI_backend/read_activity", {
+    headers: { "Content-Type": "application/json" },
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .then((out) => {
+      $.each(out, function (index, valor) {
+        nomeAtividade = valor.name;
+
+        var postElement = $(
+          '<div\
+          class="external-event bg-info"\
+          data-toggle="modal"\
+          data-target="#MaisInfo2"\
+          id="titulo"\
+          onclick="infoModal()"\
+        >\
+          Go home\
+        </div>'
+        );
+        $("#PostWrapper").append(postElement);
+        document.getElementById("titulo").innerHTML = nomeAtividade;
+        document.getElementById("titulo").id = "titulo2"; //aqui muda o id q faz com que consiga criar novo elemento
+      });
+    })
+    .catch((err) => console.error(err));
+}
+
+//como descobrir em qual é que ele carrega ? (não dá pelo ID)
+function infoModal() {
+  var teste2 = document.getElementById("titulo2").innerHTML;
+  console.log(teste2);
+  var teste = "teste2";
+  document.getElementById("nomeDaAtividade").value = teste;
+  document.getElementById("cidadeDaAtividade").value = teste2;
+  //document.getElementById("diDaAtividade").value = nomeAtividade;
+}
--- Frontend/Backoffice/Adaptado/controllers/controller_perfil.js
+++ Frontend/Backoffice/Adaptado/controllers/controller_perfil.js
@@ -4,19 +4,31 @@ window.onload = function () {
   var passwordUtilizador;
   var emailUtilizador;
 
-  fetch("http://localhost:8080/DAI_backend/read_user", {
+  //NAO TA A DAR
+  fetch("http://161.230.18.89:8080/DAI_backend/read_user", {
     headers: { "Content-Type": "application/json" },
     method: "GET",
   })
     .then((res) => res.json())
     .then((out) => {
       $.each(out, function (index, valor) {
-        nomeUtilizador = valor.username;
-        passwordUtilizador = valor.password;
-        emailUtilizador = valor.email;
-        document.getElementById("inputName").value = nomeUtilizador;
-        document.getElementById("inputName2").value = passwordUtilizador;
-        document.getElementById("inputEmail").value = emailUtilizador;
+        if (valor.id_type == 2 && valor.id_state == 1) {
+          console.log("tou aqui");
+          nomeUtilizador = valor.username;
+          passwordUtilizador = valor.password;
+          emailUtilizador = valor.email;
+          document.getElementById("inputName").value = nomeUtilizador;
+          document.getElementById("inputName2").value = passwordUtilizador;
+          document.getElementById("inputEmail").value = emailUtilizador;
+        } else if (valor.id_type == 3 && valor.id_state == 1) {
+          console.log("vim mais longe");
+          nomeUtilizador = valor.username;
+          passwordUtilizador = valor.password;
+          emailUtilizador = valor.email;
+          document.getElementById("inputName").value = nomeUtilizador;
+          document.getElementById("inputName2").value = passwordUtilizador;
+          document.getElementById("inputEmail").value = emailUtilizador;
+        }
       });
     })
     .catch((err) => console.error(err));
